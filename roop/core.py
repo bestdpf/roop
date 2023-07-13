4c62e6ec2f09ced4febac17974bd6fcb7ffb5313
duan <duan@heyi.gpu>
2023-07-13 16:04:14 +0800

    remove ui

--- roop/core.py
+++ roop/core.py
@@ -19,7 +19,7 @@ import tensorflow
 
 import roop.globals
 import roop.metadata
-import roop.ui as ui
+# import roop.ui as ui
 from roop.predicter import predict_image, predict_video
 from roop.processors.frame.core import get_frame_processors_modules
 from roop.utilities import has_image_extension, is_image, is_video, detect_fps, create_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clean_temp, normalize_output_path
@@ -133,7 +133,8 @@ def pre_check() -> bool:
 def update_status(message: str, scope: str = 'ROOP.CORE') -> None:
     print(f'[{scope}] {message}')
     if not roop.globals.headless:
-        ui.update_status(message)
+        pass
+        # ui.update_status(message)
 
 
 def start() -> None:
