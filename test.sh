30c2083d3c658d2ab26a787f0f82fa12c8811e8e
duan <duan@heyi.gpu>
2023-07-17 17:35:14 +0800

    mod test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3,4,5
 #python run.py -s ./test/black.jpg -t test/chinese-girl-dance.mp4 --keep-fps --keep-audio -o ./test-black.mp4 --execution-provider cuda
-python run.py -s ./test/marry.jpg -t test/chinese-girl-dance.mp4 --keep-fps --keep-audio -o ./test-marry.mp4 --execution-provider cuda
+# python run.py -s ./test/marry.jpg -t test/chinese-girl-dance.mp4 --keep-fps --keep-audio -o ./test-marry.mp4 --execution-provider cuda --execution-threads 1
+python run.py -s ./test/newton.jpg -t test/chinese-girl-dance.mp4 --keep-fps --keep-audio -o ./test-newton.mp4 --execution-provider cuda --execution-threads 3
